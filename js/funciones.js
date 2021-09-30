6cb5f79a8180e555a894dd586699bd0f82ed2630
martamorall <martamoral22@gmail.com>
2021-09-30 17:50:05 +0200

    first commit

---
+++ js/funciones.js
@@ -0,0 +1,18 @@
+window.onload= function() {
+    alert('window - onload');
+    let elemento=document.getElementById("nombre");
+    elemento.style.color="red"; 
+
+
+elemento.addEventListener("mouseover", function () {
+
+    elemento.style.color = "green";
+    elemento.innerText = (elemento.innerText).toUpperCase();
+
+}, false)
+elemento.addEventListener("mouseout", function () {
+    elemento.style.color = "black";
+    elemento.innerText = (elemento.innerText).toLowerCase();
+
+}, false)
+}
